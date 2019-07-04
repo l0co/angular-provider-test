a731925573614c85982e56f4cd311b1b8c607653
Łukasz Frankowski <lukasz.frankowski@gmail.com>
2019-07-04 09:05:28 +0200

    remove provided-in module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import {RouterModule, Routes} from '@angular/router';
 const routes: Routes = [
   {path: 'parent', loadChildren: () => import('./module-parent/module-parent.module').then(mod => mod.ModuleParentModule)},
   {path: 'lazy', loadChildren: () => import('./module-lazy/module-lazy.module').then(mod => mod.ModuleLazyModule)},
-  {path: 'provided-id', loadChildren: () => import('./module-provided-in/module-provided-in.module').then(mod => mod.ModuleProvidedInModule)},
   {path: '', redirectTo: 'parent', pathMatch: 'full'}
 ];
 
